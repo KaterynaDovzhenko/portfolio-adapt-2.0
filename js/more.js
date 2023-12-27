f930e31aa5bd77265ded41eafb106be9cffb26e5
KaterynaDovzhenko <kate.dovzhenk@gmail.com>
2023-12-26 22:02:24 -0500

    1st page mobile version

--- js/more.js
+++ js/more.js
@@ -4,3 +4,20 @@ element.classList.add('floating');
 
 document.getElementsByTagName("body")[0].style.overflow="hidden";
 
+// MORE BTN ABOUT
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btnMore = document.getElementById("btn-more");
+    
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnMore.innerHTML = "more"; more.style.display = "none"
+    } else {
+        dots.style.display = "none";
+        btnMore.innerHTML = "hide";
+        more.style.display = "inline";
+        btnMore.style.margin = "15px 0";
+    }
+}
