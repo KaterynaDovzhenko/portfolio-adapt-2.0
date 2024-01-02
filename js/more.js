f2a7e91829a8d3a1e7711d97618d2e1ce7289303
KaterynaDovzhenko <kate.dovzhenk@gmail.com>
2024-01-02 13:48:56 -0500

    first page done (demo) + nav port + port page

--- js/more.js
+++ js/more.js
@@ -10,15 +10,20 @@ function readMore() {
     var dots = document.getElementById("dots");
     var more = document.getElementById("more");
     var btnMore = document.getElementById("btn-more");
+    var footer = document.querySelector('.footer');
     
     if (dots.style.display === "none") {
         dots.style.display = "inline";
-        btnMore.innerHTML = "more"; more.style.display = "none"
+        btnMore.innerHTML = "more";
+        more.style.display = "none";
+        footer.style.paddingTop = "120px";
+       
     } else {
         dots.style.display = "none";
         btnMore.innerHTML = "hide";
         more.style.display = "inline";
         btnMore.style.margin = "15px 0";
+        footer.style.paddingTop = "0px";
     }
 }
 
--- js/nav.js
+++ js/nav.js
@@ -76,3 +76,19 @@ menuItems.forEach(
 //     bodyScrollLock.enableBodyScroll(document.body);
 //   });
 // })();
+
+
+const modal = document.querySelector("#modal");
+const body = document.querySelector("body");
+    
+
+  const noScroll = function (e) {
+
+      if (modal.classList.contains('is-open')) {
+          // Disable scroll
+          body.style.overflow = "hidden";
+      } else {
+          // Enable scroll
+          body.style.overflow = "auto";
+      }
+  };
